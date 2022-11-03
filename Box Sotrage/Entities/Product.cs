9e74787faab9d0a653dcd40b44867024c7323d6d
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2022-11-03 21:59:07 +0100

    update to products

--- Box Sotrage/Entities/Product.cs
+++ Box Sotrage/Entities/Product.cs
@@ -3,6 +3,13 @@
     public class Product
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
+        public int height { get; set; }
+
+        public int width { get; set; }
+
+        public string colour { get; set; }
     }
 }
